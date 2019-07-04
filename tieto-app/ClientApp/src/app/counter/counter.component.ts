131b17be26885f4155e5273a2848546f821c8a61
Wojtek <olejnik.wojciech1@gmail.com>
2019-07-04 23:58:18 +0200

    Finished feature 1 - Fibonacci sequence
    On Counter Page, I display a next element of Fibonacci Sequence to be displayed after every click.

--- tieto-app/ClientApp/src/app/counter/counter.component.ts
+++ tieto-app/ClientApp/src/app/counter/counter.component.ts
@@ -5,9 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
-  public currentCount = 0;
 
-  public incrementCounter() {
-    this.currentCount++;
+
+  public index = 0;
+  fibonacciNumber: number;
+  values = '';
+
+  public calculateFibonacciNumber() {
+    this.index++;
+    this.fibonacciNumber = this.calculate(this.index);
+    this.values += this.fibonacciNumber + ' | ';
   }
+
+  private calculate(i: number): number {
+    return (i <= 2) ? 1 : this.calculate(i - 1) + this.calculate(i - 2);
+  }
+
 }
